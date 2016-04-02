8358636cfc9adc5abf8da462b4a0f2bc3c69401b
Petar Krastanov <peter.b.krustanov@gmail.com>
2016-04-02 21:20:20 +0300

    added demo how the data could be shown

--- themes/hugo-creative-theme/static/js/demo.js
+++ themes/hugo-creative-theme/static/js/demo.js
@@ -14,21 +14,21 @@ jsPlumb.ready(function () {
 		anchors: ['Right', 'Left'],
 		connector: 'Straight'
 	});
-	
+
 	jsPlumb.connect({
 		source: 'server',
 		target: 'desktop',
 		anchors: ['Left', 'Right'],
 		connector: 'Straight'
 	});
-	
+
 	jsPlumb.connect({
 		source: 'server',
 		target: 'phone',
 		anchors: ['Left', 'Right'],
 		connector: 'Straight'
 	});
-	
+
 	jsPlumb.connect({
 		source: 'server',
 		target: 'data-input',
@@ -40,3 +40,28 @@ jsPlumb.ready(function () {
 $(window).resize(function(){
 	jsPlumb.repaintEverything();
 });
+
+function onType (e) {
+    //Check for enter keyCode
+    if (e.keyCode === 13) {
+        var element = e.target;
+
+        addText(element.value, '#phone4-list');
+        addText(element.value, '#phone3-list');
+        addText(element.value, '#phone2-list');
+        addText(element.value, '#phone1-list');
+
+        element.value = '';
+    }
+}
+
+function addText (value, elementId) {
+    var li4 = $('<li></li>');
+    li4.text(value);
+    $(elementId).append(li4);
+}
+
+$("#phone4").keypress(onType);
+$("#phone3").keypress(onType);
+$("#phone2").keypress(onType);
+$("#phone1").keypress(onType);
