9ab12e9430ed45e1d4ac84053f0a3c4129c971f8
Petar Krastanov <peter.b.krustanov@gmail.com>
2016-04-13 21:42:28 +0300

    added spots animation

--- themes/hugo-creative-theme/static/js/demo.js
+++ themes/hugo-creative-theme/static/js/demo.js
@@ -7,10 +7,16 @@
         localStorage.setItem('clientId', collectionName);
     }
 
-    var app = window.app = Neutrino.app('cf8b71c381ca4eba8b288abdb74810a9');
+    var app = window.app = Neutrino.app('2447d061c0d645edb98d171bf49c22c6');
     app.auth.login('test', 'test').then(init);
     var collection = window.collection = app.collection(collectionName);
     var objectId;
+    var macbook;
+    var iphone;
+    var htcOne;
+    var server;
+    getElementsPositions();
+    $(window).resize(getElementsPositions);
 
     function init() {
         collection.objects()
@@ -21,7 +27,7 @@
 
                 return data[0];
             })
-            .then(bind);
+            .then(bind)
     }
 
     function bind(object) {
@@ -33,8 +39,7 @@
                 var selectors = [
                     '#iphone-6-input',
                     '#mac-input',
-                    '#htc-input',
-                    '#iphone-6-plus-input'
+                    '#htc-input'
                 ];
 
                 window.app.o = realtimeObjects;
@@ -42,8 +47,10 @@
                 realtimeObjects.forEach(function (realtimeObject, index) {
                     var selector = selectors[index];
                     var el = $(selector);
+                    var elDebounce = _.debounce(_.partial(addSpotByDevice, el.prop('id')), 130);
                     el.keyup(function () {
                         realtimeObject.text = $(this).val();
+                        elDebounce();
                     });
                     realtimeObject.on(Neutrino.ObjectEvents.propertyChanged, function() {
                         el.val(realtimeObject.text);
@@ -52,6 +59,37 @@
                 });
             });
     }
+
+    function getElementsPositions() {
+        macbook = getElementPosition('macbook');
+        iphone = getElementPosition('iphone6');
+        htcOne = getElementPosition('htc-one');
+        server = getElementPosition('server');
+    }
+
+    function addSpotByDevice(device) {
+        var macbookSpot = {left: macbook.right, top: macbook.top};
+        var serverSpot = {left: server.leftCenter, top: server.topCenter};
+        var iphoneSpot = {left: iphone.right, top: iphone.bottom};
+        var htcOneSpot = {left: htcOne.left, top: htcOne.bottom};
+        switch(device) {
+            case 'mac-input':
+                addSpotAnimation(macbookSpot, serverSpot);
+                serverRandomResponse(server, iphoneSpot);
+                serverRandomResponse(server, htcOneSpot);
+                break;
+            case 'iphone-6-input':
+                addSpotAnimation(iphoneSpot, serverSpot);
+                serverRandomResponse(server, macbookSpot);
+                serverRandomResponse(server, htcOneSpot);
+                break;
+            case 'htc-input':
+                addSpotAnimation(htcOneSpot, serverSpot);
+                serverRandomResponse(server, macbookSpot);
+                serverRandomResponse(server, iphoneSpot);
+                break;
+        }
+    }
 }());
 
 $(function() {
@@ -59,3 +97,56 @@ $(function() {
         $(this).find('input').focus();
     });
 });
+
+function createElement(positions) {
+    var el = $('<div class="signal-spot"></div>');
+    $('body').append(el);
+    el.css(positions);
+    return el;
+}
+
+function getElementPosition(elementName) {
+    var element = document.getElementsByClassName(elementName)[0];
+    var elementRect = element.getBoundingClientRect();
+    return {
+        el: element,
+        right: element.offsetLeft + elementRect.width - 23,
+        left: element.offsetLeft,
+        top: element.offsetTop,
+        bottom: element.offsetTop + elementRect.height - 23,
+        leftCenter: element.offsetLeft + (elementRect.width/2) - (23/2),
+        topCenter: element.offsetTop + (elementRect.height/2) - (23/2)
+    };
+}
+
+function addSpotAnimation(spotStartPositions, spotEndPosition) {
+    var element = createElement(spotStartPositions);
+    setTimeout(function() {
+        element.css(spotEndPosition);
+
+        setTimeout(function() {
+            element.detach();
+        }, 200);
+    }, 20);
+}
+
+function serverRandomResponse(server, spotEndPosition) {
+    switch (getRandomInt(1, 4)) {
+        case 1: //Top - Left point
+            addSpotAnimation({top: server.top, left: server.left}, spotEndPosition);
+            break;
+        case 2: //Top - Right point
+            addSpotAnimation({top: server.top, left: server.right}, spotEndPosition);
+            break;
+        case 3: //Bottom - Left point
+            addSpotAnimation({top: server.bottom, left: server.left}, spotEndPosition);
+            break;
+        case 4: //Bottom - Right point
+            addSpotAnimation({top: server.bottom, left: server.right}, spotEndPosition);
+            break;
+    }
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
