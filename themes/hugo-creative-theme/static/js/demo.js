64b72b8cb17f6a4edc62d04438e116c740b6df0f
Petar Krastanov <peter.b.krustanov@gmail.com>
2016-04-18 20:05:31 +0300

    removed signal animation

--- themes/hugo-creative-theme/static/js/demo.js
+++ themes/hugo-creative-theme/static/js/demo.js
@@ -11,13 +11,7 @@
     app.auth.login('test', 'test').then(init);
     var collection = window.collection = app.collection(collectionName);
     var objectId;
-    var macbook;
-    var iphone;
-    var htcOne;
-    var server;
-    getElementsPositions();
-    $(window).resize(getElementsPositions);
-
+    
     function init() {
         collection.objects()
             .then(function (data) {
@@ -46,10 +40,8 @@
                 realtimeObjects.forEach(function (realtimeObject, index) {
                     var selector = selectors[index];
                     var el = $(selector);
-                    var elDebounce = _.debounce(_.partial(addSpotByDevice, el.prop('id')), 130);
                     el.keyup(function () {
                         realtimeObject.text = $(this).val();
-                        elDebounce();
                     });
                     realtimeObject.on(Neutrino.ObjectEvents.propertyChanged, function() {
                         el.val(realtimeObject.text);
@@ -62,37 +54,6 @@
                 });
             });
     }
-
-    function getElementsPositions() {
-        macbook = getElementPosition('macbook');
-        iphone = getElementPosition('iphone6');
-        htcOne = getElementPosition('htc-one');
-        server = getElementPosition('server');
-    }
-
-    function addSpotByDevice(device) {
-        var macbookSpot = {left: macbook.right, top: macbook.top};
-        var serverSpot = {left: server.leftCenter, top: server.topCenter};
-        var iphoneSpot = {left: iphone.right, top: iphone.bottom};
-        var htcOneSpot = {left: htcOne.left, top: htcOne.bottom};
-        switch(device) {
-            case 'mac-input':
-                addSpotAnimation(macbookSpot, serverSpot);
-                serverRandomResponse(server, iphoneSpot);
-                serverRandomResponse(server, htcOneSpot);
-                break;
-            case 'iphone-6-input':
-                addSpotAnimation(iphoneSpot, serverSpot);
-                serverRandomResponse(server, macbookSpot);
-                serverRandomResponse(server, htcOneSpot);
-                break;
-            case 'htc-input':
-                addSpotAnimation(htcOneSpot, serverSpot);
-                serverRandomResponse(server, macbookSpot);
-                serverRandomResponse(server, iphoneSpot);
-                break;
-        }
-    }
 }());
 
 $(function() {
@@ -100,56 +61,3 @@ $(function() {
         $(this).find('textarea').focus();
     });
 });
-
-function createElement(positions) {
-    var el = $('<div class="signal-spot"></div>');
-    $('body').append(el);
-    el.css(positions);
-    return el;
-}
-
-function getElementPosition(elementName) {
-    var element = document.getElementsByClassName(elementName)[0];
-    var elementRect = element.getBoundingClientRect();
-    return {
-        el: element,
-        right: element.offsetLeft + elementRect.width - 23,
-        left: element.offsetLeft,
-        top: element.offsetTop,
-        bottom: element.offsetTop + elementRect.height - 23,
-        leftCenter: element.offsetLeft + (elementRect.width/2) - (23/2),
-        topCenter: element.offsetTop + (elementRect.height/2) - (23/2)
-    };
-}
-
-function addSpotAnimation(spotStartPositions, spotEndPosition) {
-    var element = createElement(spotStartPositions);
-    setTimeout(function() {
-        element.css(spotEndPosition);
-
-        setTimeout(function() {
-            element.detach();
-        }, 200);
-    }, 20);
-}
-
-function serverRandomResponse(server, spotEndPosition) {
-    switch (getRandomInt(1, 4)) {
-        case 1: //Top - Left point
-            addSpotAnimation({top: server.top, left: server.left}, spotEndPosition);
-            break;
-        case 2: //Top - Right point
-            addSpotAnimation({top: server.top, left: server.right}, spotEndPosition);
-            break;
-        case 3: //Bottom - Left point
-            addSpotAnimation({top: server.bottom, left: server.left}, spotEndPosition);
-            break;
-        case 4: //Bottom - Right point
-            addSpotAnimation({top: server.bottom, left: server.right}, spotEndPosition);
-            break;
-    }
-
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-}
