19fe93a5efe3c13d28963433e2e59c3ea83718ed
Petar Krastanov <peter.b.krustanov@gmail.com>
2016-04-18 20:53:45 +0300

    changed app to demo and fixed signal sending

--- themes/hugo-creative-theme/static/js/demo.js
+++ themes/hugo-creative-theme/static/js/demo.js
@@ -7,8 +7,8 @@
         localStorage.setItem('clientId', collectionName);
     }
 
-    var app = window.app = Neutrino.app('2447d061c0d645edb98d171bf49c22c6');
-    app.auth.login('test', 'test').then(init);
+    var app = window.app = Neutrino.app('demo');
+    app.auth.login('demo', 'demo').then(init);
     var collection = window.collection = app.collection(collectionName);
     var objectId;
     var signalVisibilityTime = 400;
@@ -48,44 +48,25 @@
                     var el = $(selector);
                     el.keyup(function () {
                         realtimeObject.text = $(this).val();
-                        sendSignal(el);
+                        el.addClass('send-signal');
                     });
                     realtimeObject.on(Neutrino.ObjectEvents.propertyChanged, function() {
                         el.val(realtimeObject.text);
+                        el.addClass('receive-signal');
+                        setTimeout(clearSignalColors, signalVisibilityTime);
                     });
                     el.val(realtimeObject.text);
 
                     setInterval(function(){
                         el[0].scrollTop = el[0].scrollHeight;
                     }, 200);
+        
+                    function clearSignalColors() {
+                        $('.send-signal').removeClass('send-signal');
+                        $('.receive-signal').removeClass('receive-signal');
+                    }
                 });
             });
-    }
-    
-    function sendSignal(element) {
-        switch (element.selector) {
-            case selectors[0]:
-                signalElements.iphone.addClass('send-signal');
-                signalElements.macbook.addClass('receive-signal');
-                signalElements.htc.addClass('receive-signal');
-                break;
-            case selectors[1]:
-                signalElements.macbook.addClass('send-signal');
-                signalElements.iphone.addClass('receive-signal');
-                signalElements.htc.addClass('receive-signal');
-            break;
-            case selectors[2]:
-                signalElements.htc.addClass('send-signal');
-                signalElements.macbook.addClass('receive-signal');
-                signalElements.iphone.addClass('receive-signal');
-            break;            
-        }
-        setTimeout(clearSignalColors, signalVisibilityTime);
-        
-        function clearSignalColors() {
-            $('.send-signal').removeClass('send-signal');
-            $('.receive-signal').removeClass('receive-signal');
-        }
     }
 }());
 
